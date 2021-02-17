d647cd43952a72a69826b1f3344a7df430e67b09
LGXprod <mattghanno@gmail.com>
2021-02-17 12:06:25 +1100

    Converted banner to a component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from 'common/Banner';
 import "styles/index.css";
 
 function App() {
   return (
     <div>
-      
+      <Banner />
     </div>
   );
 }
---
+++ src/common/Banner.js
@@ -0,0 +1,30 @@
+import React from "react";
+import CityImage from "images/city.jpg";
+import "styles/index.css";
+
+const Banner = () => {
+  return (
+    <div className="banner-width">
+      <div id="banner-image-container" className="banner-width">
+        <div id="banner-overlay-left">
+          <div id="banner-text">
+            <h2 style={{ color: "white !important" }}>
+              INFRASTRUCTURE
+              <br />
+              REPORT
+            </h2>
+            <hr />
+          </div>
+        </div>
+        <div id="banner-overlay-right"></div>
+        <img
+          className="banner-image"
+          alt="city underpass"
+          src={CityImage}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
